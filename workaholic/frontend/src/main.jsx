fa0ec7c82567727a988aca6bf1c064fcd0e05691
vudinhan2525 <vudinhan000@gmail.com>
2024-11-20 20:43:45 +0700

    Remove node_modules

--- workaholic/frontend/src/main.jsx
+++ workaholic/frontend/src/main.jsx
@@ -1,12 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { RouterProvider } from 'react-router-dom'
-import Router from './router/Router.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import Router from "./router/Router.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={Router}/>
-  </StrictMode>,
-)
+    <RouterProvider router={Router} />
+  </StrictMode>
+);
