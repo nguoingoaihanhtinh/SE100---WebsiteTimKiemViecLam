82a8f7bb58781e2c13671362b23db6ceda82de76
vudinhan2525 <vudinhan000@gmail.com>
2024-12-06 12:11:36 +0700

    upd

--- workaholic/backend/src/routes/companyRoute.js
+++ workaholic/backend/src/routes/companyRoute.js
@@ -1,77 +1,10 @@
-import express from 'express';
-import * as CompanyController from '../controllers/companyController.js';
+import express from "express";
+import * as CompanyController from "../controllers/companyController.js";
 
 const companyRoute = express.Router();
 
-/**
- * @swagger
- * /api/companies:
- *   get:
- *     summary: Get all companies with pagination or a specific company by company_id
- *     parameters:
- *       - name: page
- *         in: query
- *         description: The page number to retrieve (default is 1)
- *         required: false
- *         schema:
- *           type: integer
- *           example: 1
- *       - name: limit
- *         in: query
- *         description: The number of companies to retrieve per page (default is 10)
- *         required: false
- *         schema:
- *           type: integer
- *           example: 10
- *      
- *     responses:
- *       200:
- *         description: List of companies or a specific company with pagination metadata
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 currentPage:
- *                   type: integer
- *                   description: The current page number
- *                   example: 1
- *                 totalPages:
- *                   type: integer
- *                   description: The total number of pages
- *                   example: 5
- *                 totalCompanies:
- *                   type: integer
- *                   description: The total number of companies
- *                   example: 50
- *                 companies:
- *                   type: array
- *                   items:
- *                     type: object
- *                     properties:
- *                       id:
- *                         type: integer
- *                         description: The company ID
- *                         example: 1
- *                       name:
- *                         type: string
- *                         description: The company name
- *                         example: Tech Corp
- *                       img:
- *                         type: string
- *                         description: The company image URL
- *                         example: "http://example.com/image.jpg"
- *                       jobAvailable:
- *                         type: integer
- *                         description: The number of available jobs in the company
- *                         example: 10
- *       500:
- *         description: Failed to fetch companies
- *   responses:
- *     500:
- *       description: Internal server error
- */
-
-companyRoute.route('/companies').get(CompanyController.getAllCompanies);
+companyRoute.get("/", CompanyController.getAllCompanies);
+companyRoute.get("/getByUser/:user_id", CompanyController.getCompanyByUserId);
+companyRoute.get("/:id", CompanyController.getCompanyById);
 
 export default companyRoute;
--- workaholic/backend/src/routes/index.js
+++ workaholic/backend/src/routes/index.js
@@ -4,6 +4,6 @@ import jobRoute from "./jobRoute.js";
 
 export default function routes(server) {
   server.use("/api/v1/job", jobRoute);
-  server.use("/api/v1", companyRoute);
+  server.use("/api/v1/company", companyRoute);
   server.use("/api/v1/user", userRouter);
 }
